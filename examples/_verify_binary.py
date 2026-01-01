48689267598b64e1cc5df7d312d38c14dc8f476a
aitazazahsan01 <aitazazahsan01@users.noreply.github.com>
2026-01-01 13:29:58 +0500

    Update _verify_binary.py

--- examples/_verify_binary.py
+++ examples/_verify_binary.py
@@ -8,3 +8,8 @@ import numpy as np
 from sklearn.datasets import make_classification
 
 from neuralnet import Sequential, DenseLayer, DropoutLayer
+from neuralnet.utils.data_utils import train_val_split, standardize
+from neuralnet.utils.metrics import classification_report_nn
+
+np.random.seed(0)
+
