33762c5e36bd446614c0dc96f1a3a276a9a34913
aitazazahsan01 <aitazazahsan01@users.noreply.github.com>
2026-01-01 12:16:43 +0500

    Update _verify_binary.py

--- examples/_verify_binary.py
+++ examples/_verify_binary.py
@@ -3,3 +3,8 @@ os.environ['MPLBACKEND'] = 'Agg'
 import sys
 sys.path.insert(0, '.')
 import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+from sklearn.datasets import make_classification
+
+from neuralnet import Sequential, DenseLayer, DropoutLayer
