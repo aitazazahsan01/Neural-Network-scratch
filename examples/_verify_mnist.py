fb498f662a6ee3e6c623fce4a07f0ac6369e521a
aitazazahsan01 <aitazazahsan01@users.noreply.github.com>
2026-01-02 11:57:34 +0500

    Update _verify_mnist.py

--- examples/_verify_mnist.py
+++ examples/_verify_mnist.py
@@ -5,3 +5,10 @@ sys.path.insert(0, '.')
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
+
+from neuralnet import Sequential, DenseLayer, DropoutLayer
+from neuralnet.utils.data_utils import train_val_split, normalize, one_hot_encode
+from neuralnet.utils.metrics import classification_report_nn
+
+np.random.seed(42)
+
